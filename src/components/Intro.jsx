f16a68a7c9566f4b5a37ffcd002cbca0a179d2d3
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-09-25 07:53:22 +0100

    Enhanced formatting for intro container

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,13 +6,10 @@ function Intro() {
       <h1>Jamie Barrett</h1>
       <p>
         Hello, I am a full stack developer with the advantage of having a
-        background in Cyber Security. I enjoy developing websites ranging from
-        business landing pages, through to data rich functional web
-        applications.
-      </p>
-      <p>
-        I have development experience using HTML, CSS, JavaScript, React,
-        Node.js, Express, SQL, Python, Google Cloud Platform and Heroku
+        background in Cyber Security. I enjoy developing all websites ranging
+        from business landing pages, through to data rich functional web
+        applications. I have development experience using HTML, CSS, JavaScript,
+        React, Node.js, Express, SQL, Python, Google Cloud Platform and Heroku
       </p>
     </div>
   );
