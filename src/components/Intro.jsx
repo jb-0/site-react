53df884bf0c87e57b383e119802e2125ccb466da
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-09-25 07:31:05 +0100

    Added a number of sections, intro, header, footer

---
+++ src/components/Intro.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Intro() {
+  return (
+    <div>
+      <h1>Jamie Barrett</h1>
+      <p>
+        Hello, I am a full stack developer with the advantage of having a
+        background in Cyber Security. I enjoy developing websites ranging from
+        business landing pages, through to data rich functional web
+        applications.
+      </p>
+      <p>
+        I have development experience using HTML, CSS, JavaScript, React,
+        Node.js, Express, SQL, Python, Google Cloud Platform and Heroku
+      </p>
+    </div>
+  );
+}
+
+export default Intro;
