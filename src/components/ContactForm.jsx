e3250402e5949785fb817318180b7c224979a332
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-09-27 13:19:47 +0100

    Finalised styling for contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -2,35 +2,37 @@ import React from "react";
 
 function ContactForm() {
   return (
-    <form class="contact-form" action="">
-      <label for="name">Name{" "}</label><br />
-      <input
-        type="text"
-        id="name"
-        name="name"
-        placeholder="Your name.."
-        className="text-input"
-      ></input><br /><br />
+    <div class="contact-form">
+      <form action="">
+        <label for="name">Name{" "}</label><br />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Your name.."
+          className="text-input"
+        ></input><br /><br />
 
-      <label for="email">Email{" "}</label><br />
-      <input
-        type="email"
-        id="email"
-        name="email"
-        placeholder="Your email.."
-        className="text-input"
-      ></input><br /><br />
-      
-      <label for="message"><span>Message{" "}</span></label><br />
-      <textarea
-        id="message"
-        name="message"
-        placeholder="Write something.."
-        className="text-input"
-      ></textarea><br /><br />
+        <label for="email">Email{" "}</label><br />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Your email.."
+          className="text-input"
+        ></input><br /><br />
+        
+        <label for="message"><span>Message{" "}</span></label><br />
+        <textarea
+          id="message"
+          name="message"
+          placeholder="Write something.."
+          className="text-input"
+        ></textarea><br /><br />
 
-      <input type="submit" value="Submit"></input>
-    </form>
+        <input type="submit" value="Submit"></input>
+      </form>
+    </div>
   );
 }
 
