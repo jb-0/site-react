f24bda1a2e27362d858347460987e71bd35b4419
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-09-28 08:58:35 +0100

    Initial commit for blog module, including creation of routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import Navbar from "./Navbar"
-import Intro from "./Intro"
-import Portfolio from "./Portfolio"
-import ContactForm from "./ContactForm";
-import Footer from "./Footer";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Navbar />
-      <Intro />
-      <Portfolio />
-      <ContactForm />
-      <Footer />
+      <Router>
+      <Route path="/" exact component={Home} />
+      {/* <Route path="/blog" exact component={Blog} />
+      <Route path="/blog/edit/:id" component={BlogPostEdit} />
+      <Route path="/blog/create" component={CreateBlogPost} /> */}
+        <Home />
+      </Router>
     </div>
   );
 }
