55043c6542ad4bd2cd197ef4f20a3a0aa5efc0cc
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-09-29 18:46:06 +0100

    Created a BlogPostForm that can be used for both Creation and Edits

--- frontend/src/components/blog/BlogPostCreate.jsx
+++ frontend/src/components/blog/BlogPostCreate.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "../common/Navbar";
 import Footer from "../common/Footer";
+import BlogPostForm from "./BlogPostForm";
 import "../../styles/Forms.css";
 
 function BlogPostCreate() {
@@ -40,39 +41,7 @@ function BlogPostCreate() {
   return (
     <div className="blog-post-create">
       <Navbar />
-      <h1>New blog post</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="title">Title</label>
-        <br />
-        <input
-          onChange={handleUpdates}
-          type="text"
-          id="title"
-          name="title"
-          placeholder="Blog title.."
-          className="text-input"
-          value={blogPost.title}
-        ></input>
-        <br />
-        <br />
-
-        <label htmlFor="post">
-          <span>Blog Post</span>
-        </label>
-        <br />
-        <textarea
-          onChange={handleUpdates}
-          id="post"
-          name="post"
-          placeholder="Write something.."
-          className="text-input"
-          value={blogPost.post}
-        ></textarea>
-        <br />
-        <br />
-
-        <input type="submit" value="Submit" className="submit-button"></input>
-      </form>
+      <BlogPostForm handleUpdates={handleUpdates} handleSubmit={handleSubmit} blogPost={blogPost}/>
       <Footer />
     </div>
   );
