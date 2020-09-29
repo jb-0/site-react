b84dc24919399d8a859384be001f6d99fa582df0
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-09-29 19:44:08 +0100

    Added edit functionality using params.id to PATCH

--- frontend/src/components/blog/BlogPostEdit.jsx
+++ frontend/src/components/blog/BlogPostEdit.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../common/Navbar";
 import Footer from "../common/Footer";
+import BlogPostForm from "./BlogPostForm";
+import "../../styles/Forms.css";
 
-function BlogPostEdit() {
+function BlogPostEdit(props) {
+  const [blogPost, setBlogPost] = useState({
+    title: "",
+    post: "",
+  });
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const url = `http://localhost:4000/blog/edit/${props.match.params.id}`;
+    console.log(url);
+
+    const rawResponse = await fetch(url, {
+    method: 'PATCH',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(blogPost)
+  });
+  //TODO err handling response
+  const content = rawResponse;
+  }
+
+  function handleUpdates(event) {
+    const target = event.target.name;
+    const value = event.target.value;
+
+    setBlogPost((previousValues) => {
+      return { ...previousValues, [target]: value };
+    });
+  }
+
   return (
     <div className="blog-post-edit">
       <Navbar />
-      <h1>Edit blog post</h1>
+      <BlogPostForm handleUpdates={handleUpdates} handleSubmit={handleSubmit} blogPost={blogPost}/>
       <Footer />
     </div>
   );
