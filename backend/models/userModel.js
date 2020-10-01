6515a53a74265e54c48a3094035b231e30a0bd90
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-01 07:44:46 +0100

    Created basic user creation with some password validation, password hashed and salted. If user exists check performed.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true, minlength: 8 },
   displayName: { type: String, required: true },
+  active: { type: Boolean, required: true, default: false },
 });
 
 const User = mongoose.model('User', userSchema);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,53 @@
 const { User } = require(`${__dirname}/../models/userModel.js`);
 const userRoutes = require('express').Router();
+const bcrypt = require('bcrypt');
 
-Router.post('/register', (req, res) => {
-  const { email, password, passwordCheck, displayName } = req.body;
-})
+userRoutes.post('/register', async (req, res) => {
+  try {
+    // A given user should supply an email, password (twice) and a display name
+    // display name will be displayed on blog posts
+    const { email, password, passwordCheck, displayName } = req.body;
+
+    // Some basic validation is performed
+    if (!email || !password || !passwordCheck || !displayName) {
+      return res.status(400).json({ msg: 'All fields must be complete' });
+    }
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .json({ msg: 'Password must be at least 8 characters' });
+    }
+    if (password !== passwordCheck) {
+      return res.status(400).json({ msg: 'Passwords must match' });
+    }
+
+    // Check to see if the user exists in the DB
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res.status(400).json({ msg: 'Existing account with this email found' });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      email,
+      password: passwordHash,
+      displayName,
+      active: false,
+    });
+
+    newUser.save((err) => {
+      if (!err) {
+        res.status(200).json({ msg: 'User created, requires activation' });
+      } else {
+        res.status(400).send('User creation failed');
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 module.exports = userRoutes;
