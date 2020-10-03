bddfe21aacdc947cacd0fb9e2a336a980ae4d5bd
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-03 18:51:44 +0100

    Added a portfolio item and updated intro content

--- frontend/src/components/home/Intro.jsx
+++ frontend/src/components/home/Intro.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import "../../styles/Intro.css";
+import React from 'react';
+import '../../styles/Intro.css';
 
 function Intro() {
   return (
@@ -7,8 +7,8 @@ function Intro() {
       <h1>Jamie Barrett</h1>
       <p>
         Hello, I am a full stack developer with a strong background in Cyber
-        Security. I enjoy developing all websites ranging from business landing
-        pages, through to data rich functional web applications. I have
+        Security. I enjoy developing all kinds of websites ranging from business
+        landing pages, through to data rich functional web applications. I have
         development experience using HTML, CSS, JavaScript, MongoDB, Express,
         React, Node.js, SQL, Python, Google Cloud Platform and Heroku.
       </p>
--- frontend/src/content/portfolioContent.js
+++ frontend/src/content/portfolioContent.js
@@ -1,9 +1,19 @@
 const portfolioContent = [
   {
+    title: "This Site",
+    content: `This site is my portfolio and blog, I built it using MongoDB, Express, Node.js and
+      React. The site includes authentication allowing me to create and edit blog posts.`,
+    links: {
+      site: "http://www.jamiebarrett.co.uk/",
+      source: "https://github.com/jb-0/site-react",
+    },
+  },
+  {
     title: "UK Property API",
     content: `This is a Node.js project that utilises web scraping (of home.co.uk) to generate an API 
       response, it also allows the API functionality to be tested on the home page as well as 
-      providing the user with an equivalent API call.`,
+      providing the user with an equivalent API call. Currently hosted on Heroku but previously
+      containerised and hosted on GCP.`,
     links: {
       site: "https://uk-property-api.herokuapp.com/",
       demo: "https://youtu.be/LlalqkidxSo",
