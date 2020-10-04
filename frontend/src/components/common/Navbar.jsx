1cdf4265e9b44199ae561095a33e3db6345f457c
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-04 19:01:37 +0100

    Added logout button which will appear only for logged in users

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,14 +1,28 @@
-import React from "react";
-import "../../styles/Navbar.css"
+import React, { useContext } from "react";
+import "../../styles/Navbar.css";
+import UserContext from '../../context/UserContext';
 
 
 function Navbar() {
+  const { userData, setUserData } = useContext(UserContext);
+
+  function handleLogout(){
+    setUserData({
+      token: undefined,
+      user: undefined
+    });
+    localStorage.setItem('auth-token', '');
+  }
+
   return (
     <div className="navbar">
       <a href="/#about">About</a>
       <a href="/#portfolio">Portfolio</a>
       <a href="/#contact">Contact</a>
       <a href="/blog">Blog</a>
+      {
+        userData.user ? <button onClick={handleLogout}>Log out</button> : null
+      }
     </div>
   );
 }
