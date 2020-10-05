64eb13ad6f22cb9ae1e86904eac3e272dea9a919
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-05 20:02:48 +0100

    Required express sslify

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+var enforce = require('express-sslify');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
