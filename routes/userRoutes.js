8ee36c65ec019410782bf25bc62dfef7f19fdef2
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-05 07:15:39 +0100

    Removed user registration route, currently no appetite for any other users and can re introduce it if ever necessary

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,58 +4,6 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const auth = require(`${__dirname}/../middleware/auth.js`);
 
-// userRoutes.post('/register', async (req, res) => {
-//   try {
-//     // A given user should supply an email, password (twice) and a display name
-//     // display name will be displayed on blog posts
-//     const { email, password, passwordCheck, displayName } = req.body;
-
-//     // Some basic validation is performed
-//     if (!email || !password || !passwordCheck || !displayName) {
-//       return res.status(400).json({ msg: 'All fields must be complete' });
-//     }
-//     if (password.length < 8) {
-//       return res
-//         .status(400)
-//         .json({ msg: 'Password must be at least 8 characters' });
-//     }
-//     if (password !== passwordCheck) {
-//       return res.status(400).json({ msg: 'Passwords must match' });
-//     }
-
-//     // Check to see if the user exists in the DB
-//     const existingUser = await User.findOne({ email });
-//     if (existingUser) {
-//       return res
-//         .status(400)
-//         .json({ msg: 'Existing account with this email found' });
-//     }
-
-//     // Hash password
-//     const salt = await bcrypt.genSalt();
-//     const passwordHash = await bcrypt.hash(password, salt);
-
-//     // Create user model
-//     const newUser = new User({
-//       email,
-//       password: passwordHash,
-//       displayName,
-//       active: false, // Active set as false to ensure only manually activated accounts can login
-//     });
-
-//     // Save the new user
-//     newUser.save((err) => {
-//       if (!err) {
-//         res.status(200).json({ msg: 'User created, requires activation' });
-//       } else {
-//         res.status(400).send('User creation failed');
-//       }
-//     });
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// });
-
 // Login route
 userRoutes.post('/login', async (req, res) => {
   try {
