c7367b00794bfa75e624c4c3415cddf2825bc3bd
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-07 20:31:30 +0100

    Started tracking window size on navbar

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,28 +1,41 @@
-import React, { useContext } from "react";
-import "../../styles/Navbar.css";
+import React, { useContext, useState, useLayoutEffect } from 'react';
+import '../../styles/Navbar.css';
 import UserContext from '../../context/UserContext';
-
 
 function Navbar() {
   const { userData, setUserData } = useContext(UserContext);
+  const [size, setSize] = useState({width: 0, height: 0});
 
-  function handleLogout(){
+  function handleLogout() {
     setUserData({
       token: undefined,
-      user: undefined
+      user: undefined,
     });
     localStorage.setItem('auth-token', '');
   }
 
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize({width: window.innerWidth, height: window.innerHeight});
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+
   return (
     <div className="navbar">
-      <a href="/#about">About</a>
-      <a href="/#portfolio">Portfolio</a>
-      <a href="/#contact">Contact</a>
-      <a href="/blog">Blog</a>
-      {
-        userData.user ? <button onClick={handleLogout}>Log out</button> : null
-      }
+      {size.width > 440 ? (
+        <div>
+          <a href="/#about">About</a>
+          <a href="/#portfolio">Portfolio</a>
+          <a href="/#contact">Contact</a>
+          <a href="/blog">Blog</a>
+        </div>
+      ) : (
+        false
+      )}
+      {userData.user ? <button onClick={handleLogout}>Log out</button> : null}
     </div>
   );
 }
