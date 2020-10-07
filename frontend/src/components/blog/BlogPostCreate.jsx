8b142633fd66583f786cfaa9878f144f9ad33a53
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-07 08:38:58 +0100

    Conditionally render blog creation dependent on logged in or not

--- frontend/src/components/blog/BlogPostCreate.jsx
+++ frontend/src/components/blog/BlogPostCreate.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
-import BlogPostForm from "./BlogPostForm";
-import "../../styles/Forms.css";
+import React, { useContext, useState } from 'react';
+import BlogPostForm from './BlogPostForm';
+import '../../styles/Forms.css';
+import UserContext from '../../context/UserContext';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import Login from '../user/Login';
 
 function BlogPostCreate() {
   const [blogPost, setBlogPost] = useState({
-    title: "",
-    author: "",
-    post: "",
+    title: '',
+    author: '',
+    post: '',
     created_date: new Date(),
   });
 
@@ -14,15 +17,15 @@ function BlogPostCreate() {
     event.preventDefault();
 
     const rawResponse = await fetch('/api/create', {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(blogPost)
-  });
-  //TODO handling response
-  const content = await rawResponse.json();
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(blogPost),
+    });
+    //TODO handling response
+    const content = await rawResponse.json();
   }
 
   function handleUpdates(event) {
@@ -35,10 +38,22 @@ function BlogPostCreate() {
   }
 
   return (
-    <div className="blog-post-create">
-      <h1>New blog post</h1>
-      <BlogPostForm handleUpdates={handleUpdates} handleSubmit={handleSubmit} blogPost={blogPost}/>
-    </div>
+    <UserContext.Consumer>
+      {(context) => {
+        if (context.userData.user) {
+          return (
+            <div className="blog-post-create">
+              <h1>New blog post</h1>
+              <BlogPostForm
+                handleUpdates={handleUpdates}
+                handleSubmit={handleSubmit}
+                blogPost={blogPost}
+              />
+            </div>
+          );
+        }
+      }}
+    </UserContext.Consumer>
   );
 }
 
