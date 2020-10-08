a69d8e1accf24d12829f2986f37daef095eccebf
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-08 07:00:21 +0100

    Added 404 page as fallback for any pages that do not exist

---
+++ frontend/src/components/common/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/Intro.css';
+
+function PageNotFound() {
+  return (
+    <div className="page-not-found" id="about">
+      <h1>404 Page not found</h1>
+    </div>
+  );
+}
+
+export default PageNotFound;
