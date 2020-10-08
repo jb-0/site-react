0ff77a0f638726bfb22626e9fe455466c6bff985
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-08 20:13:13 +0100

    Added sanitization for blog creation and loading, this allows blog html to be safely rendered with a whitelist of tags

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,6 +1,21 @@
 const { Blog } = require(`${__dirname}/../models/blogModel.js`);
 const blogRoutes = require('express').Router();
 const auth = require(`${__dirname}/../middleware/auth.js`);
+const sanitizeHtml = require('sanitize-html');
+
+const permittedHtml = {
+    allowedTags: ['b', 'i', 'em', 'strong', 'a', 'img', 'p', 'h2'],
+    allowedAttributes: {
+      a: ['href'],
+    }
+  };
+
+function sanitizeBlogPost(blogPost) {
+  const sanitizedBlogPost = blogPost;
+  sanitizedBlogPost.post = sanitizeHtml(blogPost.post, permittedHtml)
+  return sanitizedBlogPost
+}
+
 
 // Route to return all blog posts
 blogRoutes.get('/', (req, res) => {
@@ -16,25 +31,30 @@ blogRoutes.get('/', (req, res) => {
 // Route to return a specific blog post
 blogRoutes.get('/:id', (req, res) => {
   const { id } = req.params;
-  Blog.findById(id, (err, blogPost) => {
+  Blog.findById(id, (err, blogPost) => { 
     if (err) {
       console.log(err);
       res.status(404).send('blog does not exist');
     } else {
-      res.json(blogPost);
+      // Sanitize blog post to ensure only permitted HTML elements are sent over
+      // blogs also sanitized on post, but this keeps ruleset current and prevents any opportunity
+      // for disallowed HTML to be injected to the database and rendered   
+      res.json(sanitizeBlogPost(blogPost));
     }
   });
 });
 
 // Route to create a new blog post
 blogRoutes.post('/create', auth, (req, res) => {
-  const blog = new Blog({
+  let blog = new Blog({
     title: req.body.title,
     post: req.body.post,
     author: req.body.author,
     created_date: req.body.created_date,
   });
 
+  blog = sanitizeBlogPost(blog);
+  
   blog.save((err) => {
     if (!err) {
       res.status(200).json({ blog: 'blog created', content: blog });
@@ -56,7 +76,7 @@ blogRoutes.patch('/edit/:id', auth, (req, res) => {
       } else {
         res.send(err);
       }
-    },
+    }
   );
 });
 
