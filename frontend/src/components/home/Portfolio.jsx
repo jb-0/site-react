4da41c5942990298f7e458729fbe86a8510f49e1
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-10-10 13:36:22 +0100

    Differentiate the card size minimums for mobile versus desktop

--- frontend/src/components/home/Portfolio.jsx
+++ frontend/src/components/home/Portfolio.jsx
@@ -1,17 +1,26 @@
-import React from "react";
-import Card from "./Card";
-import portfolioContent from "../../content/portfolioContent";
-import "../../styles/Portfolio.css"
+import React, { useContext } from 'react';
+import Card from './Card';
+import portfolioContent from '../../content/portfolioContent';
+import '../../styles/Portfolio.css';
+import { ViewContext } from '../../context/ViewContext';
 
 function Portfolio() {
+  const size = useContext(ViewContext);
+  
   return (
     <div className="portfolio" id="portfolio">
-        <h1>Portfolio</h1>
-        <div className="portfolio-cards-section">
+      <h1>Portfolio</h1>
+      <div
+        className={
+          size.width > 440
+            ? 'portfolio-cards-section'
+            : 'portfolio-cards-section mobile'
+        }
+      >
         {portfolioContent.map((portfolioItem, key) => {
-            return <Card key={key} item={portfolioItem} />;
+          return <Card key={key} item={portfolioItem} />;
         })}
-        </div>
+      </div>
     </div>
   );
 }
