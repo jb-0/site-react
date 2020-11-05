210e82cbef058eb8fde49ab179994afeab8ca250
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-05 12:03:06 +0000

    Removed portfolio item and changed contact form inputs background colour

--- frontend/src/content/portfolioContent.js
+++ frontend/src/content/portfolioContent.js
@@ -42,15 +42,6 @@ const portfolioContent = [
     },
   },
   {
-    title: "Books",
-    content: `Using Node.js, HTML and CSS I built a site that interacts with the nytimes books api
-        and allows users to select a genre and view charting books in the selected genre.`,
-    links: {
-      site: "https://bookcharts.herokuapp.com/",
-      source: "https://github.com/jb-0/books",
-    },
-  },
-  {
     title: "Continuous Deployment Cloud Function",
     content: `To achieve continuous deployment for a static website hosted in a Google Cloud Storage
         Bucket, I developed a Cloud Function in Python. The Google Cloud Function is triggered via
