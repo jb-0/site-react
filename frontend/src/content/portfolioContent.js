dcda3ce85ef7277911a98b798aa7bc38655634c3
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-09 06:31:52 +0000

    Updates to portfolio wording

--- frontend/src/content/portfolioContent.js
+++ frontend/src/content/portfolioContent.js
@@ -1,17 +1,17 @@
 const portfolioContent = [
   {
-    title: "Skill Search (In Progress)",
+    title: "Skill Search (In Progress) - Full Stack",
     content: `This site helps jobs seekers track skill demand in their area, the backend is complete
-    and the frontend is currently in progress. The site includes Passport authentication via Google
-    and Facebook allowing user's to save searches relevant to them. It was built using MongoDB,
-    Express, React and Node.js. Backend tests created using Mocha and Chai, with frontend tests
-    created using Jest and React Testing Library. `,
+    and the frontend is actively being enhanced to improve functionality. The site includes Passport 
+    authentication via Google and Facebook allowing user's to save searches relevant to them. It was 
+    built using MongoDB, Express, React and Node.js. Backend tests created using Mocha and Chai, 
+    with frontend tests created using Jest and React Testing Library.`,
     links: {
       source: "https://github.com/jb-0/skill-search",
     },
   },
   {
-    title: "This Site",
+    title: "This Site - Full Stack",
     content: `This site is my portfolio and blog, I built it using MongoDB, Express, React and
     Node.js. Tests created using Mocha and Chai. The site includes authentication allowing me to
     create and edit blog posts. I've also implemented rate limiting to prevent undesired activity.`,
@@ -21,7 +21,7 @@ const portfolioContent = [
     },
   },
   {
-    title: "UK Property API",
+    title: "UK Property API - Full Stack",
     content: `This is a Node.js project that utilises web scraping (of home.co.uk) to generate an API
       response, it also allows the API functionality to be tested on the home page as well as
       providing the user with an equivalent API call. Currently hosted on Heroku but previously
@@ -33,7 +33,7 @@ const portfolioContent = [
     },
   },
   {
-    title: "Data Science: Twitter NLP",
+    title: "Twitter NLP - Data Science",
     content: `Using NLP and unsupervised learning I was able to predict a number of key events
       in a football match using tweets. This work was performed using Python, Jupyter, Anaconda and
       packages such as: pandas, matplotlib, sklearn, nltk and wordcloud.`,
@@ -42,7 +42,7 @@ const portfolioContent = [
     },
   },
   {
-    title: "Continuous Deployment Cloud Function",
+    title: "Continuous Deployment - Cloud Function",
     content: `To achieve continuous deployment for a static website hosted in a Google Cloud Storage
         Bucket, I developed a Cloud Function in Python. The Google Cloud Function is triggered via
         pushes to the GitHub repo (using a webhook).`,
