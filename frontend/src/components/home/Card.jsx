4efac6e077ee5bf0a68dd97377bf414e3478aaf1
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-30 20:34:38 +0000

    Added functionality to "read more" on a given portfolio item

--- frontend/src/components/home/Card.jsx
+++ frontend/src/components/home/Card.jsx
@@ -1,9 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import LanguageIcon from '@material-ui/icons/Language';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import YouTubeIcon from '@material-ui/icons/YouTube';
 import { ViewContext } from '../../context/ViewContext';
 import '../../styles/Card.css';
+import { v4 as uuidv4 } from 'uuid';
 
 function Card(props) {
   const size = useContext(ViewContext);
@@ -40,15 +41,27 @@ function Card(props) {
     }
   }
 
+  function handleClick(event) {
+    // If the card is already selected then collapse it, otherwise set it as the selected card
+    if (parseInt(props.selectedCardId, 10) === props.id) {
+      props.setSelectedCardId(undefined);
+    } else {
+      props.setSelectedCardId(event.target.id);
+    }
+  }
+
   return (
-    <div className="card-item">
+    <div
+      className={parseInt(props.selectedCardId, 10) === props.id ? 
+      'card-item selected-portfolio-item' : 'card-item'}
+    >
       <h3>{props.item.title}</h3>
       <p>{props.item.content}</p>
       <button
+        id={props.id}
+        onClick={handleClick}
         className={
-          size.width > 440
-            ? 'read-more-button'
-            : 'read-more-button-mobile'
+          size.width > 440 ? 'read-more-button' : 'read-more-button-mobile'
         }
       >
         Read More
@@ -59,7 +72,7 @@ function Card(props) {
           const linkName = key;
 
           return (
-            <span className="card-link">
+            <span key={uuidv4} className="card-link">
               <a href={link}>{getIcon(linkName)}</a>{' '}
             </span>
           );
--- frontend/src/components/home/Portfolio.jsx
+++ frontend/src/components/home/Portfolio.jsx
@@ -1,12 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Card from './Card';
 import portfolioContent from '../../content/portfolioContent';
 import '../../styles/Portfolio.css';
 import { ViewContext } from '../../context/ViewContext';
+import { v4 as uuidv4 } from 'uuid';
 
 function Portfolio() {
   const size = useContext(ViewContext);
-  
+  const [selectedCardId, setSelectedCardId] = useState();
+
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
@@ -18,7 +20,15 @@ function Portfolio() {
         }
       >
         {portfolioContent.map((portfolioItem, key) => {
-          return <Card key={key} item={portfolioItem} />;
+          return (
+            <Card
+              item={portfolioItem}
+              id={key}
+              key={uuidv4()}
+              selectedCardId={selectedCardId}
+              setSelectedCardId={setSelectedCardId}
+            />
+          );
         })}
       </div>
     </div>
