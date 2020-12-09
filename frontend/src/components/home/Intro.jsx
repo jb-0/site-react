46a00ec309250ee5896b4bb8a2aae423ccdcde67
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-12-09 08:20:00 +0000

    Made my name into spans and animated them to drop down on load

--- frontend/src/components/home/Intro.jsx
+++ frontend/src/components/home/Intro.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../../styles/Intro.css';
 
 function Intro() {
+  const name = 'Jamie Barrett';
+  const nameSpans = name.split('').map((letter, index) => {
+    let classNames = 'name'
+    if (letter === ' ') classNames = `${classNames} space`
+
+    return (<><span
+      className={classNames}
+      style={{ 'animation-delay': `${index * 100}ms` }}
+    >
+      {letter}
+    </span>{letter === ' ' ? <br></br> : null}</>
+    )
+  });
+
   return (
     <div className="intro" id="about">
-      <h1>Jamie Barrett</h1>
+      <h1>{nameSpans}</h1>
+
       <p>
         Hello, I am a full stack developer with six years' experience in Cyber
         Security. I enjoy developing all kinds of websites ranging from business
