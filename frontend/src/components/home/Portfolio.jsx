a6e52fc89add0dfcd5cb520ad3534e6168b6ca4a
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-01-12 07:56:45 +0000

    Applied styling to prevent scroll on background (behind modal popup)

--- frontend/src/components/home/Portfolio.jsx
+++ frontend/src/components/home/Portfolio.jsx
@@ -9,9 +9,19 @@ import { v4 as uuidv4 } from 'uuid';
 function Portfolio() {
   const size = useContext(ViewContext);
   const [selectedCardId, setSelectedCardId] = useState();
+  
+  // When a card is selected we want to prevent scroll on background
+  if (selectedCardId) {
+    document.body.style.height = '100vh';
+    document.body.style.overflowY = 'hidden';
+  } else {
+    document.body.style.height = null;
+    document.body.style.overflowY = null;
+  }
 
   return (
-    <div className="portfolio" id="portfolio">
+    <div className={`portfolio ${selectedCardId ? 'detailed-portfolio-open' : ''}`} 
+     id="portfolio">
       <h1>Portfolio</h1>
       <div
         className={
