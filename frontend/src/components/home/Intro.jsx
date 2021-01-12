2087b26749c91e913de651cd4559afa7e3135b6b
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-01-12 07:30:36 +0000

    Removed name animation

--- frontend/src/components/home/Intro.jsx
+++ frontend/src/components/home/Intro.jsx
@@ -2,24 +2,9 @@ import React, { useRef } from 'react';
 import '../../styles/Intro.css';
 
 function Intro() {
-  const firstName = 'Jamie';
-  const lastName = 'Barrett';
-  
-  const firstNameSpans = firstName.split('').map((letter, index) => {
-    return (<><span
-      className="first-name"
-      style={{ 'animation-delay': `${index * 150}ms` }}
-    >
-      {letter}
-    </span>{letter === ' ' ? <br></br> : null}</>
-    )
-  });
-
-  const fullName = <>{firstNameSpans}<br/><span className="last-name">{lastName}</span></>
-
   return (
     <div className="intro" id="about">
-      <h1>{fullName}</h1>
+      <h1>Jamie Barrett</h1>
 
       <p>
         Hello, I am a full stack developer with six years' experience in Cyber
