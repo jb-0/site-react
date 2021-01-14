da406f16955fa7eb01fd66b80f619b475e70fea1
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-01-14 06:35:43 +0000

    One more minor tweak to intro

--- frontend/src/components/home/Intro.jsx
+++ frontend/src/components/home/Intro.jsx
@@ -7,7 +7,7 @@ function Intro() {
       <h1>Jamie Barrett</h1>
 
       <p>
-        I am a Controls Specialist and Automation Developer with 6 years’ experience in technology assurance roles. I also have full stack development experience and enjoy developing websites ranging from business landing pages, through to data rich functional web applications. I have development experience using HTML, CSS, JavaScript, MongoDB, Express, React, Node.js, SQL, Python, Google Cloud Platform and Heroku.
+        I am a Controls Specialist and Automation Developer with 6 years’ experience in technology assurance and data analytics roles. I also have full stack development experience and enjoy developing websites ranging from business landing pages, through to data rich functional web applications. I have development experience using HTML, CSS, JavaScript, MongoDB, Express, React, Node.js, SQL, Python, Google Cloud Platform and Heroku.
       </p>
     </div>
   );
