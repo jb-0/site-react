cab192961e400c6c13a1d289d638a53c07a5afba
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-01-26 19:12:10 +0000

    Removed frontend elements that refer to unused functionality

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,14 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Home from './home/Home';
-import Blog from './blog/Blog';
-import BlogPostEdit from './blog/BlogPostEdit';
-import BlogPostCreate from './blog/BlogPostCreate';
-import BlogPost from './blog/BlogPost';
-import Login from './user/Login';
 import Navbar from './common/Navbar';
 import Footer from './common/Footer';
 import PageNotFound from './common/PageNotFound';
-import {UserProvider} from '../context/UserContext';
 import {ViewProvider} from '../context/ViewContext'
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -19,14 +13,12 @@ function App() {
     <div className="app">
       <Router>
         <ViewProvider>
-        <UserProvider>
           <Navbar />
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="*" component={PageNotFound} status={404} />
           </Switch>
           <Footer />
-        </UserProvider>
         </ViewProvider>
       </Router>
     </div>
